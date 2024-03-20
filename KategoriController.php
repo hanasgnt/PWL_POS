178df6d4756ebb8129cb4d8060fef2fd9e39ab04
Hana Sugianto <117093924+hanasgnt@users.noreply.github.com>
2024-03-21 00:18:58 +0700

    Add files via upload

---
+++ KategoriController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index(KategoriDataTable $dataTable)
+    {
+        return $dataTable->render('kategori.index');
+    }
+    public function create()
+    {
+        return view('kategori.create');
+    }
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
+    }
+    public function edit($id)
+    {
+        $kategori = KategoriModel::find($id);
+        return view('kategori.edit', ['data' => $kategori]);
+    }
+    public function edit_save($id, Request $request)
+    {
+        $kategori = KategoriModel::find($id);
+
+        $kategori->kategori_kode = $request->kodeKategori;
+        $kategori->kategori_nama = $request->namaKategori;
+
+        $kategori->save();
+
+        return redirect('/kategori');
+    }
+    public function delete($id)
+    {
+        $kategori = KategoriModel::findOrFail($id);
+        $kategori->delete();
+        return redirect('/kategori');
+    }
+}
---
+++ edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layout.app')
+
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Kategori')
+@section('content_header_title', 'Kategori')
+@section('content_header_subtitle', 'Create')
+{{-- Content body: main page content --}}
+@section('content')
+    <div class="container">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Buat Kategori Baru</h3>
+            </div>
+            <form method="POST" action="../edit_save/{{ $data->kategori_id }}">
+                {{ csrf_field() }}
+                {{ method_field('PUT') }}
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="kodeKategori">Kode Kategori</label>
+                        <input type="text" class="form-control" id="kodeKategori" name="kodeKategori"
+                            placeholder="Kode Kategori" value="{{ $data->kategori_kode }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="namaKategori">Nama Kategori</label>
+                        <input type="text" class="form-control" id="namaKategori" name="namaKategori"
+                            placeholder="Kode Kategori" value="{{ $data->kategori_nama }}">
+                    </div>
+                </div>
+                {{-- <a href="../../kategori" class="btn btn-secondary btn-sm">Back</button> --}}
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Ubah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
