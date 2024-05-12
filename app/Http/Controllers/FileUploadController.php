65be11c90fab4776baec41ccf9404fc6311f5d1f
hana sugianto <hanasugianto2006@gmail.com>
2024-05-12 11:35:42 +0700

    P12 Praktikum E - Membuat Symlink

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -12,35 +12,22 @@ class FileUploadController extends Controller
     }
     public function prosesFileUpload(Request $request)
     {
-        // dump($request->berkas);
-        // dump($request->file('berkas'));
-        // return "Pemrosesan file upload di sini";
-
-        /*if ($request->hasFile('berkas')) {
-            echo "path(): " . $request->berkas->path();
-            echo "<br>";
-            echo "extension(): " . $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): " . $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): " . $request->berkas->getSize();
-        } else {
-            echo "Tidak ada berkas yang diupload";
-        }*/
-
         $request->validate([
             'berkas' => 'required|file|image|max:500',
         ]);
 
+        // Penamaan file
         $extfile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-' . time() . "." . $extfile;
+
+        // Path file
         // $path = $request->berkas->store('uploads');
-        $path = $request->berkas->storeAs('uploads', $namaFile);
+        $path = $request->berkas->storeAs('public', $namaFile);
+
+        $pathBaru = asset('storage/' . $namaFile);
         echo "Proses upload berhasil, file berada di: $path";
+        echo "<br>";
+        echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
         // echo $request->berkas->getClientOriginalName() . "lolos validasi";
     }
 }
