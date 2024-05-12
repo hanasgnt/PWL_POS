625021db4181a4408ebdbf1c5b747d4a3a9b21fc
hana sugianto <hanasugianto2006@gmail.com>
2024-05-12 20:51:22 +0700

    P12 Tugas 1 - Update

--- app/Http/Controllers/FileUploadRenameController.php
+++ app/Http/Controllers/FileUploadRenameController.php
@@ -13,18 +13,20 @@ class FileUploadRenameController extends Controller
     public function prosesFileUpload(Request $request)
     {
         $request->validate([
+            'nama_berkas' => 'required|alpha_dash',
             'berkas' => 'required|file|image|max:500',
         ]);
 
         // Penamaan file
+        // $namaFile = $request->nama_berkas . "." . $request->berkas->getClientOriginalExtension();
         $namaFile = $request->nama_berkas . "." . $request->berkas->extension();
 
         // Path file
-        // $path = $request->berkas->storeAs('public', $namaFile);
+        // $path = $request->berkas->storeAs('public/gambar', $namaFile);
         $path = $request->berkas->move('gambar', $namaFile);
         $path = str_replace("\\", "//", $path);
 
-        // $pathBaru = asset('storage/' . $namaFile);
+        // $pathBaru = asset('storage/gambar' . $namaFile);
         $pathBaru = asset('gambar/' . $namaFile);
         echo "Gambar berhasil diupload ke <a href='$pathBaru'>$namaFile</a>";
         echo "<br><br>";
